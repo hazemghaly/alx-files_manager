9d283083c782578e2193202ddf42f13577d03dca
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-11 08:16:22 +0300

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,26 @@
+import MongoClient from 'mongodb/lib/mongo_client';
+
+class DBClient {
+    constructor() {
+        this.host = process.env.DB_HOST || 'localhost';
+        this.port = process.env.DB_PORT || 27017;
+        this.database = process.env.DB_DATABASE || 'files_manager';
+        this.url = `mongodb://${this.host}:${this.port}`;
+        this.client = new MongoClient(this.url);
+    }
+
+    isAlive() {
+        return this.client.isConnected();
+    }
+    
+    async nbUsers() {
+        return this.client.db(this.database).collection('users').countDocuments();
+    }
+
+    async nbFiles() {
+        return this.client.db(this.database).collection('files').countDocuments();
+    }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
