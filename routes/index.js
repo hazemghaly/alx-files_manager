52e811fc73cf40ac2fd1d250967d93019f5d1ad9
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-15 06:00:55 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
-// const express = require('express');
-// const app = express.Router();
-// app.get('/status', setup.create);
-// app.get('/stats', setup.create);
-
-// module.exports = app;
 const { Router } = require('express');
+const AppController = require('../controllers/AppController');
 
 const router = new Router();
 
-router.route('/status').get((req, res) => {
-  res.send({});
-});
-router.route('/stats').get((req, res) => {
-  res.send({});
-});
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
+// const { Router } = require('express');
+
+// const router = new Router();
+
+// router.route('/status').get((req, res) => {
+//   res.send({});
+// });
+// router.route('/stats').get((req, res) => {
+//   res.send({});
+// });
 
 module.exports = router;
