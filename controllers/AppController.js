a65d4be6d266c52a33e54614c2d6e99d21bb0823
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-15 06:31:25 +0300

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,7 +5,7 @@ import redisClient from '../utils/redis';
 const setup = (app) => {
   app.get('/status', (req, res) => {
     if (redisClient.isAlive() && dbClient.isAlive()) {
-      res.status(200).send('{"redis":true,"db":true}');
+      res.status(200).send('{ "redis": true,"db": true }');
     } else {
       res.status(500).send(Error, `not connected : ${Error}`);
     }
@@ -13,7 +13,7 @@ const setup = (app) => {
   app.get('/stats', async (req, res) => {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
-    res.status(200).send(`{"users":${users},"files":${files}}`);
+    res.status(200).send(`{ "users": ${users},"files": ${files} }`);
   });
 };
 
