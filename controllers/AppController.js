296934ca94b5a701101c0bafcd61a81cd59789a4
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-15 05:36:04 +0300

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,6 +1,6 @@
-import '../server';
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+import './server';
+import dbClient from './utils/db';
+import redisClient from './utils/redis';
 
 const setup = (app) => {
   app.get('/status', (req, res) => {
@@ -8,8 +8,10 @@ const setup = (app) => {
       res.status(200).send('{ "redis": true, "db": true }');
     }
   });
-  app.get('/stats', (req, res) => {
-    res.status(200).send(`{ "users": ${dbClient.users}, "files": ${dbClient.files} }`);
+  app.get('/stats', async (req, res) => {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).send(`{ "users": ${users}, "files": ${files} }`);
   });
 };
 
