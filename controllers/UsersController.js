a0e91e80564fdfe41bd25150ecee07692b8a0bae
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-17 01:45:42 +0300

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,21 +1,40 @@
-import '../server';
-import dbClient from '../utils/db';
+import mongoose from 'mongoose';
+// import sha1 from 'sha1';
+// import '../server';
+import crypto from 'crypto';
 import { sha1 } from '../node_modules/sha1';
+import dbClient from '../utils/db';
+import '../server';
 
-exports.postNew = async (req, res) => {
-    if (req.email && req.password && dbClient.isAlive()) {
-        const collection = dbClient.db(dbClient.database).collection('users');
-        if (!req.email){
-            res.status(400).send({"error":"Missing email"});
-        }
-        if (!req.password) {
-            res.status(400).send({"error":"Missing password"});
-        }
-        if (await collection.findOne({email: req.email})) {
-            res.status(400).send({"error":"Already exist"})
-        }
-        new_user = await collection.insertOne({ "email": req.email , "password": sha1(req.password)})
-        console.log(new_user)
-        res.status(201).send('HI')
-    }
+mongoose
+  .connect(dbClient.url, { useNewUrlParser: true, useUnifiedTopology: true });
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+const User = mongoose.model('User', UserSchema);
+
+const postNew = async (req, res) => {
+  const { email, password } = req.body;
+  const secretKey = '';
+  const concatenated = req.password + secretKey;
+  const securedpassword = crypto.createHash('SHA1').update(concatenated).digest('hex');
+  if (!email) {
+    return res.status(400).send('{"error":"Missing email"}');
+  }
+  if (!password) {
+    return res.status(400).send('{"error":"Missing password"}');
+  }
+  const existingUser = await User.findOne({ email: req.email });
+  if (existingUser) {
+    return res.status(400).send('{"error":"Already exist"}');
+  }
+  const newuser = new User({
+    email,
+    password: securedpassword,
+  });
+  await newuser.save();
+  console.log(newuser.email);
+  return res.status(201).send({ id: newuser.id, email: newuser.email });
 };
+module.exports = { postNew };
