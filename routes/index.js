0042b7e6131957e23cd9e32b69f313402048b78d
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-16 04:37:01 +0300

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const { Router } = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const UsersController = require('../controllers/UsersController');
+
 
 const router = new Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+
+router.conncet('/connect', AuthController.getConnect)
+router.disconnect('/disconnect', AuthController.getDisconnect)
+router.disconnect('/users/me', UsersController.getMe)
 
 module.exports = router;
