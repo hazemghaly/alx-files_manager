f1f2a3ae1087570f2671a63bf210dedd881350aa
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-18 02:21:27 +0300

    Create a new user

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const routes = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use('/', routes);
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', routes);
 
 module.exports = app;
