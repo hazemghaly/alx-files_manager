f0f38abbd4b14801a2f6b53c400b44f0d7a4f9e6
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-18 01:37:02 +0300

    Create a new user

--- server.js
+++ server.js
@@ -6,12 +6,9 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(routes);
-
 app.listen(port, hostname, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
