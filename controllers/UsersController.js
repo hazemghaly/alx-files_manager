ab3359b64c93460badc7cb922f6f00baa5b80498
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-22 01:23:29 +0300

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,16 +5,16 @@ class UsersController {
   static async postNew(request, response) {
     const { email, password } = request.body;
     if (!email) {
-      return response.status(400).send('{"error":"Missing email"}');
+      response.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      return response.status(400).send('{"error":"Missing password"}');
+      response.status(400).json({ error: 'Missing password' });
     }
     const hashPwd = sha1(password);
     const collection = dbClient.client.db(dbClient.database).collection('users');
     const user1 = await collection.findOne({ email });
     if (user1) {
-      response.status(400).json({ error: 'Already exist' });
+      response.status(400).send('{"error":"Already exist"}');
     }
     collection.insertOne({ email, password: hashPwd });
     const newUser = await collection.findOne(
